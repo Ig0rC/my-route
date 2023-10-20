eb447a5044b8b96c03ca903a00d28a9d8e0d1f88
Igor <igorcardosod3@gmail.com>
2023-10-20 16:54:51 -0300

    feat(Employee): add new annotations to validation

--- src/main/java/br/dev/igorcardoso/myroute/entitys/Employee.java
+++ src/main/java/br/dev/igorcardoso/myroute/entitys/Employee.java
@@ -1,23 +1,40 @@
 package br.dev.igorcardoso.myroute.entitys;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import br.dev.igorcardoso.myroute.useCases.employeeUseCase.DTOs.CreateEmployeeDTO;
 
 import java.util.UUID;
 
-@Entity(name = "employees")
 @Data
+@Entity(name = "employees")
+@Table(name = "employees")
 public class Employee {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-    @Column(name = "full_name")
+    @Column(name = "full_name", nullable = false, length = 180)
     private String fullName;
+    @Column(length = 180)
     private String position;
+    @Column(length = 180)
     private String registration;
-    private String lost_number;
+    @Column(name = "lot_number", length = 180)
+    private String lotNumber;
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private String createdAt;
+
+    public Employee(CreateEmployeeDTO employeeDTO) {
+        this.fullName = employeeDTO.fullName();
+        this.position = employeeDTO.position();
+        this.registration = employeeDTO.registration();
+        this.lotNumber = employeeDTO.lotNumber();
+    }
 }
