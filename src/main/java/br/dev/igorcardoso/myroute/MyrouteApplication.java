02d417d1cb54c9707893d35b5807c33eb18b4e68
Igor <igorcardosod3@gmail.com>
2023-10-20 10:08:27 -0300

    initial config

---
+++ src/main/java/br/dev/igorcardoso/myroute/MyrouteApplication.java
@@ -0,0 +1,13 @@
+package br.dev.igorcardoso.myroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/igorcardoso/myroute/MyrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.igorcardoso.myroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
