d012f9dcc6fe3601929c60167912376160474483
Igor <igorcardosod3@gmail.com>
2023-10-20 16:52:12 -0300

    feat(User): add new annotations to validation

--- src/main/java/br/dev/igorcardoso/myroute/entitys/User.java
+++ src/main/java/br/dev/igorcardoso/myroute/entitys/User.java
@@ -1,25 +1,40 @@
 package br.dev.igorcardoso.myroute.entitys;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import org.hibernate.annotations.CreationTimestamp;
+
+import br.dev.igorcardoso.myroute.useCases.userUseCase.DTOs.CreateUserDTO;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false, length = 180)
     private String email;
+
+    @Column(nullable = false, length = 180)
     private String password;
+
+
+    @Column(name = "created_at")
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public User(CreateUserDTO user) {
+        this.email =  user.email();
+        this.password = user.password();
+    }
 }
