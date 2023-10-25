c5b56a58e657ab58fce98c84b65f33607e6e22d8
Ig0rC <igorcardosod3@gmail.com>
2023-10-24 22:11:19 -0300

    fix: alter type attribute for ServiceMonth entity

--- src/main/java/br/dev/igorcardoso/myroute/entitys/ServiceMonth.java
+++ src/main/java/br/dev/igorcardoso/myroute/entitys/ServiceMonth.java
@@ -1,23 +1,36 @@
 package br.dev.igorcardoso.myroute.entitys;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.LocalDate;
-
+import br.dev.igorcardoso.myroute.useCases.serviceMonthUseCase.DTOs.CreateServiceMonthDTO;
+import java.util.Date;
+import java.util.UUID;
 
 @Entity(name = "services_month")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServiceMonth {
     @Id
     @GeneratedValue(generator = "UUID")
-    private String id;
+    private UUID id;
+
     @Column(name = "date_month", nullable = false)
-    private LocalDate dateMonth;
-    @ManyToOne
-    @JoinColumn(name = "car_id", nullable = false)
-    private Car car;
+    private Date dateMonth;
+
+    // @ManyToOne
+    @Column(name = "car_id")
+    private UUID carId;
 
     @Column(name = "created_at")
     @CreationTimestamp
     private String createdAt;
+
+    public ServiceMonth(CreateServiceMonthDTO createServiceMonthDTO) {
+        this.dateMonth = createServiceMonthDTO.dateMonth();
+        this.carId = createServiceMonthDTO.carId();
+    }
 }
