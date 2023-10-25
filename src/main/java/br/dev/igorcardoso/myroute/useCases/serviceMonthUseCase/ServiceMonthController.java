2785ab3a2433d3bcf2c24c960420e3d396019c0f
Ig0rC <igorcardosod3@gmail.com>
2023-10-24 22:06:47 -0300

    feat: Created ServiceMonthController

---
+++ src/main/java/br/dev/igorcardoso/myroute/useCases/serviceMonthUseCase/ServiceMonthController.java
@@ -0,0 +1,23 @@
+package br.dev.igorcardoso.myroute.useCases.serviceMonthUseCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.dev.igorcardoso.myroute.useCases.serviceMonthUseCase.DTOs.CreateServiceMonthDTO;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/services-month")
+public class ServiceMonthController {
+
+    @Autowired
+    CreateServiceMonthUseCase createServiceMonthUseCase;
+    
+    @PostMapping
+    public void create(@RequestBody @Valid CreateServiceMonthDTO body) {
+        this.createServiceMonthUseCase.execute(body);
+    }
+}
