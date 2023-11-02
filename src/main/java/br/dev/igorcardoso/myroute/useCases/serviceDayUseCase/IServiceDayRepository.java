84c5881caafb4d6356fd0643063a4a4716757cfe
Ig0rC <igorcardosod3@gmail.com>
2023-11-02 11:29:45 -0300

    refactor: Moving the repositories to the 'repositories' package

--- src/main/java/br/dev/igorcardoso/myroute/useCases/serviceDayUseCase/IServiceDayRepository.java
+++ src/main/java/br/dev/igorcardoso/myroute/useCases/serviceDayUseCase/IServiceDayRepository.java
@@ -5,7 +5,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.dev.igorcardoso.myroute.entitys.ServiceDay;
+import java.time.LocalDate;
+import java.util.List;
+
+
 
 public interface IServiceDayRepository extends JpaRepository<ServiceDay, UUID> {
-    
+    List<ServiceDay> findByServiceDate(LocalDate serviceDate);
 }
