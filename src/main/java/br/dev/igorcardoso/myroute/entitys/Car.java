ab4104a685887800f575f025ac845f0c573bd806
Ig0rC <igorcardosod3@gmail.com>
2023-11-02 11:10:39 -0300

    feat: finalized Car Entity

--- src/main/java/br/dev/igorcardoso/myroute/entitys/Car.java
+++ src/main/java/br/dev/igorcardoso/myroute/entitys/Car.java
@@ -5,19 +5,41 @@ import lombok.Data;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import br.dev.igorcardoso.myroute.useCases.carUseCase.DTOs.CreateCarDTO;
+
 @Entity(name = "cars")
 @Data
 public class Car {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 180)
     private String plate;
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 4)
+    private String year;
+
+    @Column(nullable = false, length = 180)
     private String mark;
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 180)
     private String model;
-    @OneToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private String createdAt;
+
+    public Car(CreateCarDTO carDTO) {
+        this.plate = carDTO.plate();
+        this.mark = carDTO.mark();
+        this.model = carDTO.model();
+        this.year = carDTO.year();
+        this.userId = carDTO.userId();
+    }
 }
