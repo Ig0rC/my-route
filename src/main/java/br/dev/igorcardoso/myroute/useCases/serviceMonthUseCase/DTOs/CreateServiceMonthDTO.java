1fc2741777cd6c31b523b9939acc47c355f4f855
Ig0rC <igorcardosod3@gmail.com>
2023-11-02 11:42:21 -0300

    chore(CreateServiceMonthDTO): Change 'Date' attribute to 'LocalDate'

--- src/main/java/br/dev/igorcardoso/myroute/useCases/serviceMonthUseCase/DTOs/CreateServiceMonthDTO.java
+++ src/main/java/br/dev/igorcardoso/myroute/useCases/serviceMonthUseCase/DTOs/CreateServiceMonthDTO.java
@@ -1,13 +1,13 @@
 package br.dev.igorcardoso.myroute.useCases.serviceMonthUseCase.DTOs;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.UUID;
 
 import jakarta.validation.constraints.NotNull;
 
 public record CreateServiceMonthDTO(
     @NotNull
-    Date dateMonth,
+    LocalDate dateMonth,
     @NotNull
     UUID carId
 ) {
