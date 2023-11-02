fb4038f354d8ff67406123134c593dabf1cd5a3f
Ig0rC <igorcardosod3@gmail.com>
2023-11-02 11:10:18 -0300

    feat: finalized CarController

--- src/main/java/br/dev/igorcardoso/myroute/useCases/carUseCase/CarController.java
+++ src/main/java/br/dev/igorcardoso/myroute/useCases/carUseCase/CarController.java
@@ -1,4 +1,28 @@
 package br.dev.igorcardoso.myroute.useCases.carUseCase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.dev.igorcardoso.myroute.useCases.carUseCase.DTOs.CreateCarDTO;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/cars")
 public class CarController {
+
+  @Autowired
+  private CreateCarUseCase createCarUseCase;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody @Valid CreateCarDTO carDTO) {
+
+    this.createCarUseCase.execute(carDTO);
+
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
 }
